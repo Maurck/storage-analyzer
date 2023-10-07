94c9537c5d1afcf71b8af6a1d33d47d6fec6468d
Maurck <mau9977@gmail.com>
2023-10-07 01:12:46 -0500

    feat: implemented basic layout

---
+++ src/components/header/HeaderComponent.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+export const HeaderComponent = () => {
+    return (
+        <div className={"c-header"}>
+
+        </div>
+    );
+}
+
+export default HeaderComponent;
+

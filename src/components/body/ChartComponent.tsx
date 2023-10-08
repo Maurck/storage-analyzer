ffe389dc7c327adf6ad03868863c5a5cff0941de
Maurck <mau9977@gmail.com>
2023-10-08 16:03:28 -0500

    feat: implemented box design

--- src/components/body/ChartComponent.tsx
+++ src/components/body/ChartComponent.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 export const ChartComponent = () => {
     return (
-        <div className={"c-chart"}>
-
+        <div className={"c-chart-container"}>
+            <div className={"c-chart complete-box-shadow"}></div>
         </div>
     );
 }
